65854a3e1cfc5c15a8275a8e9f695544f071fc0f
Calvin Lin <62937414+principal-gorilla@users.noreply.github.com>
2020-12-18 09:59:31 +1100

    remove unnecessary libraries

--- src/main/java/com/yuxiong/tech/logincomponent/LogincomponentApplication.java
+++ src/main/java/com/yuxiong/tech/logincomponent/LogincomponentApplication.java
@@ -1,18 +1,19 @@
 package com.yuxiong.tech.logincomponent;
 
+import org.apache.logging.log4j.ThreadContext;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-
 
 @SpringBootApplication
 @MapperScan("com.yuxiong.tech.logincomponent.dao")
 public class LogincomponentApplication {
 
     public static void main(String[] args) {
+        // Get the process id
+        long id = ProcessHandle.current().pid();
+        ThreadContext.put("pid", Long.toString(id));
 
         SpringApplication.run(LogincomponentApplication.class, args);
     }
-
 }
--- src/main/java/com/yuxiong/tech/logincomponent/auth/service/MyUserDetailsService.java
+++ src/main/java/com/yuxiong/tech/logincomponent/auth/service/MyUserDetailsService.java
@@ -3,19 +3,16 @@ package com.yuxiong.tech.logincomponent.auth.service;
 import com.yuxiong.tech.logincomponent.dao.UserMapper;
 import com.yuxiong.tech.logincomponent.entity.User;
 import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.ThreadContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import org.apache.logging.log4j.Logger;
 
-import java.lang.management.ManagementFactory;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,22 +24,12 @@ public class MyUserDetailsService implements UserDetailsService {
 
     private static Logger log = LogManager.getLogger(MyUserDetailsService.class);
 
-    static {
-        // Get the process id
-        String pid = ManagementFactory.getRuntimeMXBean().getName();
-        System.out.println(pid);
-        long id = ProcessHandle.current().pid();
-        System.out.println(id);
-        ThreadContext.put("pid", Long.toString(id));
-        System.out.println(ThreadContext.get("pid"));
-    }
-
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
         User user = userMapper.findByUsername(username);
 
-        log.info(username);
+        log.info("Login username: " + username);
         if(user == null)
             throw new UsernameNotFoundException("User does not exist in database");
 
@@ -51,7 +38,7 @@ public class MyUserDetailsService implements UserDetailsService {
             auths.add(new SimpleGrantedAuthority("ROLE_"+role));
         }
 
-        log.info(user.getPassword());
+        log.info("User password: " + user.getPassword());
         return new org.springframework.security.core.userdetails.User(username, user.getPassword(), auths);
     }
 }
